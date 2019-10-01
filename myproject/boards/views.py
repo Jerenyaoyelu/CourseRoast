ecc74aecb357512dc767e73e37da2d43439f05bc
YAOYE LU <luyaoyejeren@gmail.com>
2019-10-01 17:56:57 +1000

    find the part of code which causes error, need to figure out why

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -35,70 +35,59 @@ def board_topics(request,pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # todo: get the currently logged in user
+    print("as")
+    print(board,user)
+    print("as")
+    #works fine
+    # if request.method == 'POST':
+    #     subject = request.POST['subject']
+    #     message = request.POST['message']
+
+    #     #create Topic instance
+    #     topic = Topic.objects.create(
+    #         subject=subject,
+    #         board=board,
+    #         starter=user
+    #     )
+
+    #     #create Post instance
+    #     post = Post.objects.create(
+    #         message=message,
+    #         topic=topic,
+    #         created_by=user
+    #     )
+
+    #     return redirect('board_topics', pk=board.pk)  # todo: redirect to the created topic page
+
+    # return render(request, 'new_topic.html', {'board': board})
+
+    #works fine
+    # if request.method == 'POST':
+    #     form = NewTopicForm(request.POST)
+    #     if form.is_valid():
+    #         topic = form.save(commit=False)
+    #         topic.board = board
+    #         topic.starter = user
+    #         topic.save()
+    #         post = Post.objects.create(
+    #             message=form.cleaned_data.get('message'),
+    #             topic=topic,
+    #             created_by=user
+    #         )
+    #         return redirect('board_topics', pk=board.pk)  #todo: redirect to the created topic page
+    # else:
+    #     form = NewTopicForm()
+    # return render(request, 'new_topic.html', {'board': board, 'form': form})
+
+    #does not work
+    #Error: NOT NULL constraint failed: boards_topic.board_id
+#     #grab data from html and start a new topic
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first()  # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
-
-    return render(request, 'new_topic.html', {'board': board})
-
-
-    #if there is no users created, this will occur error message
-#     user = User.objects.first()
-#     print('as')
-#     print(board)
-#     print(user)
-#     print('as')
-
-#     # if request.method == 'POST':
-#     #     form = NewTopicForm(request.POST)
-#     #     if form.is_valid():
-#     #         topic = form.save(commit=False)
-#     #         topic.board = board
-#     #         topic.starter = user
-#     #         topic.save()
-#     #         post = Post.objects.create(
-#     #             message=form.cleaned_data.get('message'),
-#     #             topic=topic,
-#     #             created_by=user
-#     #         )
-#     #         return redirect('board_topics', pk=board.pk)  #todo: redirect to the created topic page
-#     # else:
-#     #     form = NewTopicForm()
-#     # return render(request, 'new_topic.html', {'board': board, 'form': form})
-# # def new_topic(request,pk):
-# #     board=get_object_or_404(Board,pk=pk)
-# #     user=User.objects.first()
-
-# #     #grab data from html and start a new topic
-#     if request.method == 'POST':
-#         form=NewTopicForm(request.POST)
-#         if form.is_valid():
-#             # topic=form.save(commit=False)
-#             # topic.board = board
-#             # topic.starter = user
-#             topic=form.save()
-#             # post=Post.objects.create(
-#             #     message=message,
-#             #     topic=topic,
-#             #     created_by=user
-#             # )
-#             return redirect('board_topics',pk=board.pk)
-#     else:
-#         form=NewTopicForm()
-#     return render(request,'new_topic.html',{'board':board,'form':form})
+        form=NewTopicForm(request.POST)
+        if form.is_valid():
+            topic=form.save()
+            return redirect('board_topics',pk=board.pk)
+    else:
+        form=NewTopicForm()
+    return render(request,'new_topic.html',{'form':form})
