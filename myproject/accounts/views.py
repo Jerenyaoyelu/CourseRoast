4c34f8d0d9ae439178ac47e9a436b6b655697fd7
YAOYE LU <luyaoyejeren@gmail.com>
2019-10-09 16:14:14 +1100

    Displaying Menu For Authenticated Users

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login as auth_login
 from .forms import SignUpForm
 
 def signup(request):
@@ -8,7 +8,7 @@ def signup(request):
         form=SignUpForm(request.POST)
         if form.is_valid():
             user=form.save()
-            login(request,user)
+            auth_login(request,user)
             return redirect('home')
     else:
         form=SignUpForm()
